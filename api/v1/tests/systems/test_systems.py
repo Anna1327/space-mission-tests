0a65ae4ab14783f3d15bc310a5f8885be7c6091f
Anna1327 <aoi.yatsu@ya.ru>
2026-05-30 12:47:10 +0300

    add negative tests for systems

--- api/v1/tests/systems/test_systems.py
+++ api/v1/tests/systems/test_systems.py
@@ -6,18 +6,13 @@ data = SystemsData()
 auth_data = AuthData()
 
 
+# POSITIVE TESTS
 @allure.feature("Systems checks")
 @allure.title("Get all system's list (GET /api/v1/systems/)")
 @pytest.mark.asyncio
 async def test_get_systems_returns_200(authentic_client):
     response = await authentic_client.get("/api/v1/systems/")
     assert response.status_code == 200
-
-# GET /systems без токена (Негативный):
-# Дёрнуть эндпоинт через твоего unauthentic_client и проверить, что он железно возвращает 401 Unauthorized.
-
-# GET /systems с тухлым токеном (Негативный):
-# Вручную записать в client.token = "fake_token" и проверить, что бэк не пускает (снова 401).
 
 
 @allure.feature("Systems checks")
@@ -82,15 +77,6 @@ async def test_get_system_by_id_returns_200(authentic_client, created_system):
     assert response.status_code == 200
 
 
-@allure.feature("Systems checks")
-@allure.title("Get system by id negative (GET /api/v1/systems/{system_id})")
-@pytest.mark.asyncio
-async def test_get_system_by_id_returns_404(authentic_client):
-    system_id = 999999
-    response = await authentic_client.get(f"/api/v1/systems/{system_id}")
-    assert response.status_code == 404
-
-
 @allure.feature("Systems trigger checks")
 @allure.title("Add trigger event to system (POST /api/v1/systems/{system_id}/trigger/{event_type})")
 @pytest.mark.asyncio
@@ -109,6 +95,35 @@ async def test_add_trigger_event_to_system(authentic_client, created_system, eve
     get_system = await authentic_client.get(f"/api/v1/systems/{created_system['id']}")
     system_data = get_system.json()
     assert system_data["status"] == expected_status
+
+
+# NEGATIVE TESTS
+@allure.feature("Systems checks")
+@allure.title("Get all system's list negative (GET /api/v1/systems/)")
+@pytest.mark.asyncio
+async def test_get_systems_without_token(unauthentic_client):
+    response = await unauthentic_client.get("/api/v1/systems/")
+    assert response.status_code == 401
+
+
+@allure.feature("Systems checks")
+@allure.title("Get all system's list negative (GET /api/v1/systems/)")
+@pytest.mark.asyncio
+async def test_get_systems_with_fake_token(unauthentic_client):
+    unauthentic_client.token = "this_is_a_fake_and_broken_jwt_token"
+    response = await unauthentic_client.get("/api/v1/systems/")
+    assert response.status_code == 401
+    data = response.json()
+    assert data["detail"] == "Invalid or expired token"
+
+
+@allure.feature("Systems checks")
+@allure.title("Get system by id negative (GET /api/v1/systems/{system_id})")
+@pytest.mark.asyncio
+async def test_get_system_by_id_returns_404(authentic_client):
+    system_id = 999999
+    response = await authentic_client.get(f"/api/v1/systems/{system_id}")
+    assert response.status_code == 404
 
 
 @allure.feature("Systems trigger checks")
